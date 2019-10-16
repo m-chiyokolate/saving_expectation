67d716bd8cb95c7cbceca434e1c8ac214063f3a0
Misato Furutani <m_furutani@yumemi.co.jp>
2019-10-16 15:27:49 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MainPage />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import MainPage from "./components/MainPage";
+
+@Component({
+  components: {
+    MainPage
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 40px 30px;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-form-group label="income:" label-for="income" label-cols="1">
+      <b-form-input v-model="income" id="income"></b-form-input>
+    </b-form-group>
+    <b-form-group label="expense:" label-for="expense" label-cols="1">
+      <b-form-input v-model="expense" id="expense"></b-form-input>
+    </b-form-group>
+    <b-input v-model="spanMonth" style="width:60px; display: inline"></b-input
+    >ヵ月分表示
+    <div style="margin-top: 10px"><b-button @click="calc">calc</b-button></div>
+    <hr />
+    {{ result }} 円
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class MainPage extends Vue {
+  income: number = 0;
+  expense: number = 0;
+  spanMonth: number = 1;
+
+  result: number = 0;
+
+  calc() {
+    this.result = (this.income - this.expense) * this.spanMonth;
+  }
+}
+</script>
+
+<style scoped></style>
